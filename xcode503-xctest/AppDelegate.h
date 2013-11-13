88da09ef3e383c2ab14023b206206d28e7de9136
Heiko Behrens <HeikoBehrens@gmx.de>
2013-11-13 12:09:49 +0100

    initial commit

---
+++ xcode503-xctest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xcode503-xctest
+//
+//  Created by Heiko Behrens on 13.11.13.
+//  Copyright (c) 2013 Heiko Behrens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xcode503-xctest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  xcode503-xctest
+//
+//  Created by Heiko Behrens on 13.11.13.
+//  Copyright (c) 2013 Heiko Behrens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
