027f5524a9b5e8d572f3df5d65a205b74728c3e8
Heiko Behrens <HeikoBehrens@gmx.de>
2013-11-13 12:24:56 +0100

    sentests on Xcode and xctool

---
+++ xcode503-xctestTests/xcode463_sentestTests.h
@@ -0,0 +1,13 @@
+//
+//  xcode463_sentestTests.h
+//  xcode463-sentestTests
+//
+//  Created by Heiko Behrens on 13.11.13.
+//  Copyright (c) 2013 Heiko Behrens. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface xcode463_sentestTests : SenTestCase
+
+@end
---
+++ xcode503-xctestTests/xcode463_sentestTests.m
@@ -0,0 +1,32 @@
+//
+//  xcode463_sentestTests.m
+//  xcode463-sentestTests
+//
+//  Created by Heiko Behrens on 13.11.13.
+//  Copyright (c) 2013 Heiko Behrens. All rights reserved.
+//
+
+#import "xcode463_sentestTests.h"
+
+@implementation xcode463_sentestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in xcode463-sentestTests");
+}
+
+@end
